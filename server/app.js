b4715afe1a9a96e895877c9f5e75a4ed1c495dcc
Jaspreet Singh <jaspreetsingh44555@gmail.com>
2024-05-28 11:30:19 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ import cros from "cors";
 
 // const MONGODB_URL = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@mymongoinit.6md0cxy.mongodb.net/gemini?retryWrites=true&w=majority`;
 const MONGODB_URL = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.rjlzzfk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-// const PORT_NO = 3030;
+const PORT_NO = process.env.PORT;
 
 const app = express();
 
